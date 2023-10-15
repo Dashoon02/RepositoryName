76907794c16872a833ecc1a010f57b6975db945b
Dasha <79089072781@mail.ru>
2023-10-15 21:56:36 +0300

    clean main

--- Лабораторная работа 1/task_4.py
+++
@@ -1,9 +0,0 @@
-users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
-
-# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
-unique_users = set(users)
-dictionary = {
-"Общее количество": len(users),
-"Уникальные посещения": len(unique_users)
-}
-print(dictionary)
